edfa555e4675988d2a4bf9fe583940960d788e18
Marcus Kelly <mek413@gmail.com>
2019-11-02 15:39:00 -0500

    finshed logic to compare for the best friend match

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -4,19 +4,18 @@ var friendArray = [
     {
     "name":"John",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-        ]
-    }
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+    "name":"Barry",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,3,4,2,5,5,3,2,1,2]
+    },
+    {
+    "name":"Allison",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[4,2,1,3,5,1,2,2,3,4]
+    },
 ];
 
 module.exports = friendArray;
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -7,7 +7,37 @@ module.exports = function(app) {
     });
     // api post for survey
     app.post("/api/friends", function(req,res){
-        friendData.push(req.body);
-        res.json(req.body);
+        user = req.body
+        // turns user score into integers
+        for (var i=0;i<user.scores.length;i++){
+            user.scores[i]= parseInt(user.scores[i]);
+        }
+
+        // friend match is default at first index
+        var match = 0
+
+        // difference is set at the max it can be so it can lower to the best match
+        var matchPoints = 40
+
+        // loop through friendData array to start the matching
+        for (var i=0;i<friendData.length;i++){
+            // setting default difference of each index in friendData array
+            var totalDiff = 0
+
+            for (var j=0;j<friendData[i].scores.length;j++){
+                var scoreDiff = Math.abs(user.scores[j] - friendData[i].scores[j]);
+                totalDiff += scoreDiff;
+            }
+
+        }
+        // checks for lowest difference to match user and friend from array
+        if (totalDiff < matchPoints){
+            match = i;
+            matchPoints = totalDiff
+        }
+
+
+        friendData.push(user);
+        res.json(friendData[match]);
     });
 }
